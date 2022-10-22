eab4d8fb2a529efff559f2108002517bf653560d
Aruzhan Muratbek <aruzhan_1106@mail.ru>
2022-10-22 09:32:45 +0600

    Second commit

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0xc4a16B616bdfF2F6Ae100E16f5BEF935Bc7d430C";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
